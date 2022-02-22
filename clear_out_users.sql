92bc90ca7fb2e0b401d213bd5d9351c1c2033405
MarkyMan4 <markus.hutnik@gmail.com>
2022-02-21 20:53:20 -0600

    delete service requests when clearing users

--- clear_out_users.sql
+++ clear_out_users.sql
@@ -1,6 +1,7 @@
 -- this deletes all non-admin users and their associated data, can be helpful to clear out your local database sometimes
 -- replace 12 with the ID of your admin user
 delete from knox_authtoken;
+delete from agency_api_servicerequest;
 delete from agency_api_staffmember;
 delete from agency_api_caretaker;
 delete from agency_api_securityquestionanswer where user_id <> 12;
