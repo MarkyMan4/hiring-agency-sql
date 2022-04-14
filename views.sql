3bb8652decec1e01ad16c9666ab17d08e759aed1
MarkyMan4 <markus.hutnik@gmail.com>
2022-04-13 21:37:10 -0500

    replaced tabs with spaces

--- views.sql
+++ views.sql
@@ -1,38 +1,38 @@
 create or replace view agency_api_pendingpayment as (
-	with work_history as (
-		select 
-			healthcare_professional_id,
-			sum(extract(epoch from (end_time - start_time)) / 60 / 60) as hours_worked
-		from agency_api_serviceentry
-		group by healthcare_professional_id
-	),
-	payment_history as (
-		select 
-			healthcare_professional_id,
-			sum(amount) as amt_paid
-		from agency_api_payment
-		group by healthcare_professional_id
-	),
-	money_owed as (
-		select 
-			usr.id as user_id,
-			hp.id as healthcare_professional_id,
-			usr.first_name || ' ' || usr.last_name as full_name,
-			usr.username,
-			hp.hourly_rate,
-			wh.hours_worked,
-			ph.amt_paid,
-			(hp.hourly_rate * wh.hours_worked) - ph.amt_paid as amt_owed
-		from
-			agency_api_healthcareprofessional hp
-			inner join auth_user usr
-				on hp.user_id = usr.id
-			inner join work_history wh
-				on hp.id = wh.healthcare_professional_id
-			inner join payment_history ph
-				on hp.id = ph.healthcare_professional_id
-	)
-	select *
-	from money_owed
-	where amt_owed > 0
+    with work_history as (
+        select 
+            healthcare_professional_id,
+            sum(extract(epoch from (end_time - start_time)) / 60 / 60) as hours_worked
+        from agency_api_serviceentry
+        group by healthcare_professional_id
+    ),
+    payment_history as (
+        select 
+            healthcare_professional_id,
+            sum(amount) as amt_paid
+        from agency_api_payment
+        group by healthcare_professional_id
+    ),
+    money_owed as (
+        select 
+            usr.id as user_id,
+            hp.id as healthcare_professional_id,
+            usr.first_name || ' ' || usr.last_name as full_name,
+            usr.username,
+            hp.hourly_rate,
+            wh.hours_worked,
+            ph.amt_paid,
+            (hp.hourly_rate * wh.hours_worked) - ph.amt_paid as amt_owed
+        from
+            agency_api_healthcareprofessional hp
+            inner join auth_user usr
+                on hp.user_id = usr.id
+            inner join work_history wh
+                on hp.id = wh.healthcare_professional_id
+            inner join payment_history ph
+                on hp.id = ph.healthcare_professional_id
+    )
+    select *
+    from money_owed
+    where amt_owed > 0
 );
